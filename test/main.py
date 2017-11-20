49b212242807a05ecbe3468806257b39f267d663
Chongyang Chen <chongyang.chen@nexusguard.com>
2017-11-20 22:41:46 +0800

    add

---
+++ test/main.py
@@ -0,0 +1,20 @@
+urls=[
+ "/web-development/"
+,"/programming/"
+,"/datebases/"
+,"/graphics-design/"
+,"/operating-systems/"
+,"/networking-cloud-computing/"
+,"/administration/"
+,"/certification/"
+,"/computers-technology/"
+,"/enterprise/"
+,"/game-programming/"
+,"/hardware/"
+,"/marketing-seo/"
+,"/security/"
+,"/software/"
+]
+url="http://www.allitebooks.com/"
+for types in urls:
+    print types
